91f0abbdce30e12d3c6c2dc9a19b7b1d3aa7acc0
safinhr <hrsafin2434@gmail.com>
2024-01-31 23:29:07 +0600

    create 2 screen view website

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,45 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import MainLayout from "./Layout/MainLayout";
-function App() {
+import React, { useEffect, useState } from 'react';
+import axios from "axios"
+import "./App.css"
+import ShowDetails from './Components/ShowDetails/ShowDetails';
+import ShowList from './Components/ShowLists/ShowList';
+import TicketForm from './Components/TicketForm/TicketForm';
+const App = () => {
+  const [shows, setShows] = useState([])
+  const [selectShow, setSelectShow] = useState(null)
+  const [bookingData, setBookingData] = useState(null)
 
-  const router = new createBrowserRouter([
-    {
-      path : "/",
-      element : <MainLayout />
-    }
-  ])
-  
+  useEffect(() => {
+    axios.get("https://api.tvmaze.com/search/shows?q=all")
+    .then(res => {
+      setShows(res.data)
+      console.log(res.data)
+    })
+    .catch((error) => console.log(error))
+  }, [])
+
+  const handleShow = (show) => {
+    setSelectShow(show)
+  }
+
+  const handleBookShow = (show) => {
+    setSelectShow(null)
+    setBookingData(show)
+  }
+
+  const handleTicketSubmit = (userData) => {
+    setBookingData(null)
+    console.log("Ticket Booked", {...bookingData, userData})
+  }
   return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
+    <div className='app'>
+      {selectShow && <ShowDetails show={selectShow.show} onBookingTicket={handleBookShow} />}
+
+      {!selectShow && !bookingData && <ShowList shows={shows} onShowClick={handleShow} />}
+
+      {bookingData && <TicketForm show={bookingData} onSubmit={handleTicketSubmit} />}
+    </div>
   );
-}
+};
 
 export default App;
---
+++ vite-project/src/Components/TicketForm/TicketForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const TicketForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TicketForm;
+
+
+
+
