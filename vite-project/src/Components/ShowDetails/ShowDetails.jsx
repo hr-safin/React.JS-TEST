8e8f306e6e4411eedcc1c72d9447af40a490ead2
safinhr <hrsafin2434@gmail.com>
2024-01-31 23:35:14 +0600

    show details page

--- vite-project/src/Components/ShowDetails/ShowDetails.jsx
+++ vite-project/src/Components/ShowDetails/ShowDetails.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const ShowDetails = () => {
-    return (
-        <div>
-            
+const ShowDetails = ({ show, onBookingTicket }) => {
+  return (
+    <div className="show-details">
+      <h2>Show Details</h2>
+      <div className="card" style="width: 18rem;">
+        <img src={show.img?.medium} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{show.name}</h5>
+          <p className="card-text">
+            Type : {show.type}
+          </p>
+          <p className="card-text">
+            Summary : {show.summary}
+          </p>
+          
+          <button onClick={()=> onBookingTicket(show)} className="btn btn-primary">
+            Book Ticket
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShowDetails;
